b0e3dc6930221c246e572c5d361efe1b00a163f2
ebonansea <emilianobonansea@gmail.com>
2014-01-21 15:26:42 -0200

    Update setup.sh
    Se agregan dev library de lua y hwloc.

--- setup.sh
+++ setup.sh
@@ -1,12 +1,16 @@
 # derived from http://trafficserver.readthedocs.org/en/latest/admin/getting-started.en.html#before-you-start
 sudo apt-get update
-sudo apt-get install vim pkg-config libtool build-essential gcc make openssl tcl tcl-dev expat libpcre3-dev libcap-dev flex hwloc lua5.2 libncurses5-dev curl git autoconf automake libxml2-dev
+sudo apt-get install vim pkg-config libtool build-essential gcc make openssl tcl tcl-dev expat libpcre3-dev libcap-dev flex hwloc libhwloc-dev lua5.1 liblua5.1-dev libncurses5-dev curl git autoconf automake libxml2-dev
+
+mkdir /opt/ats
+adduser tserver
+chown tserver:tserver /opt/ats
 
 git clone https://git-wip-us.apache.org/repos/asf/trafficserver.git
 
 cd trafficserver
 autoreconf -if
-./configure --prefix=/opt/ats --with-user=tserver
+./configure --prefix=/opt/ats --with-user=tserver --with-openssl
 make
 make check
 make install
